5dd1001e4cbd9d90280807044af63b44268e078c
Stephen Liedig <sliedig@amazon.com>
2018-04-08 12:58:04 +0800

    UI fixes for 2web. Vue model update to match step function events. Fixes for template.

--- steps/registerIncident/dev/lambda_function.py
+++ steps/registerIncident/dev/lambda_function.py
@@ -3,10 +3,10 @@ import os
 import uuid
 from datetime import datetime
 import boto3
-from aws_xray_sdk.core import patch_all, xray_recorder
+# from aws_xray_sdk.core import patch_all, xray_recorder
 
 # Patch all supported libraries for X-Ray - More info: https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
-patch_all()
+# patch_all()
 
 incidents_table = os.environ['TABLE_NAME']
 dynamodb = boto3.resource('dynamodb')
@@ -15,25 +15,23 @@ table = dynamodb.Table(incidents_table)
 
 def lambda_handler(event, context):
 
-    new_id = str(uuid.uuid4())
-    incident_date = datetime.strptime(
-        event['incident_date'], "%Y-%m-%d").strftime('%Y-%m-%dT%H:%M:%SZ')
+    incident_date = datetime.strptime(event['incident_date'], "%Y-%m-%d").strftime('%Y-%m-%dT%H:%M:%SZ')
     student_id = event['student_id']
 
-    return create_incident(new_id, incident_date, student_id)
+    return create_incident(incident_date, student_id)
 
-
-@xray_recorder.capture('put_item')
-def create_incident(iid, incident_date, student_id):
+#@xray_recorder.capture('put_item')
+def create_incident(incident_date, student_id):
 
     # Persist new incident
     new_incident = {
-        "incident_id": iid,
+        "incident_id": str(uuid.uuid4()),
         "incident_date": incident_date,
         "student_id": student_id,
         "incident_resolved": False
     }
-    response = table.put_item(
+
+    table.put_item(
         Item=new_incident
     )
 
